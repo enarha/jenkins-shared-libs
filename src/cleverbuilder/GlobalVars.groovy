44dc6cf59cc3c0a5c0e789f860fefeeff5795af7
Emil Natan <ena@redhat.com>
2019-02-07 15:17:55 +0200

    Initial commit
    Signed-off-by: Emil Natan <ena@redhat.com>

---
+++ src/cleverbuilder/GlobalVars.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+// src/com/cleverbuilder
+
+package com.cleverbuilder
+
+class GlobalVars {
+    static String foo="bar"
+}
---
+++ vars/sayHello.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+def Call(String name='human') {
+    echo "Hello ${name}"
+}
+
